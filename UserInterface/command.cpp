27377cec67d7bcd0d33c787406331d880afabea1
mgavryliuk <michaelgavriluk1@gmail.com>
2019-07-05 07:54:12 +0300

    upd commandparser

--- UserInterface/command.cpp
+++ UserInterface/command.cpp
@@ -1,11 +1,76 @@
+#include <iostream>
 #include "command.h"
 
-std::string Command::exec(const Command::args_t &)
+void Command::exec(const Command::args_t& pArgs)
 {
-    return "";
+    Command::arg_values_t arg_values(mArgs_info.size());
+
+    for(size_t i = 0; i < arg_values.size(); ++i)
+    {
+        arg_values[i] = mArgs_info[i].default_value;
+    }
+
+    bool flag_with_name = false;
+    bool flag_execute = true;
+    for (auto& arg : pArgs)
+    {
+
+        if (isNamedArgument(arg))
+        {
+            flag_with_name = true;
+            int32_t arg_pos = getArgPosition(arg);
+
+            if (arg_pos < 0)
+            {
+                std::cout << "[ERROR]: invalid argument name!";
+                flag_execute = false;
+                break;
+            }
+
+            arg_values[static_cast<size_t>(arg_pos)] = extractValue(arg);
+        }
+        else if (isOptionon(arg))
+        {
+            flag_with_name = true;
+            int32_t arg_pos = getOptionPosition(arg[1]);
+
+            if (arg_pos < 0)
+            {
+                std::cout << "[ERROR]: invalid argument name!";
+                flag_execute = false;
+                break;
+            }
+
+            arg_values[static_cast<size_t>(arg_pos)] = extractValue(arg);
+        }
+        else
+        {
+            if(flag_with_name)
+            {
+                std::cout << "[ERROR]: Argument can't be unnamed after any other named!";
+                flag_execute = false;
+                break;
+            }
+
+        }
+
+    }
+
 }
 
 const std::string& Command::getName() const
 {
     return mName;
 }
+
+bool Command::isOptionon(const Command::arg_t &pArg)
+{
+    return pArg[0] == '-' && pArg[2] == '=';
+}
+
+bool Command::isNamedArgument(const Command::arg_t &pArg)
+{
+    return pArg[0] == '-' && pArg[1] == '-';
+}
+
+
--- UserInterface/commandparser.h
+++ UserInterface/commandparser.h
@@ -10,13 +10,10 @@ class CommandParser
     using command_map = std::map<std::string, Command>;
 
 public:
-    const std::string RESP_EMPTY_STRING = "";
-    const std::string RESP_UNKNOWN_COMMAND = "";
-
     CommandParser() = default;
 
     void add_command(const Command& pCommand);
-    const std::string& parse_string(const std::string& pCommand_str);
+    void parse_string(const std::string& pCommand_str);
 
 private:
     command_map mCommands;

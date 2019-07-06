654b4bfc9c7b4f498e1f2d461bb58a503b931d13
mgavryliuk <michaelgavriluk1@gmail.com>
2019-07-06 08:14:41 +0300

    upd UserInterface

--- UserInterface/command.h
+++ UserInterface/command.h
@@ -1,39 +1,21 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
-#include <string>
-#include <vector>
-
+#include <QString>
+#include <QStringList>
 class Command
 {
 public:
-    using arg_t = std::string;
-    using args_t = std::vector<arg_t>;
-    using arg_values_t = std::vector<std::string>;
-
     Command() = default;
+    virtual ~Command() = default;
 
-    void exec(const args_t& pArgs);
-    const std::string& getName() const;
+    void exec(const QString& pArgs);
+    const QString& getName() const;
 
 private:
-    std::string mName;
+    virtual void call_cxx_function(const QStringList& pArg_list);
 
-    struct ArgumentInfo
-    {
-        std::string name;
-        char option_name;
-        std::string help;
-        std::string default_value;
-    };
-
-    std::vector<ArgumentInfo> mArgs_info;
-
-    bool isOptionon(const arg_t& pArg);
-    bool isNamedArgument(const arg_t& pArg);
-    int32_t getArgPosition(const arg_t& pArg);
-    int32_t getOptionPosition(char pOpt);
-    std::string extractValue(const arg_t& pArg);
+    QString mName;
 };
 
 #endif // COMMAND_H

0d8203a72ecc9f2a5ecbe1a19287a5b75ef5aa5e
mgavryliuk <michaelgavriluk1@gmail.com>
2019-07-13 16:49:41 +0300

    add server

--- main.cpp
+++ main.cpp
@@ -1,26 +1,11 @@
 #include <QCoreApplication>
-#include "UserInterface/interface.h"
-#include <qiostream.h>
-#include <functional>
-
-class A
-{
-public:
-    void foo(QString a, QString b, QString c)
-    {
-        QIO::qout << a << '\n'
-                  << b << '\n'
-                  << c << '\n';
-        QIO::qout.flush();
-    }
-};
-
+#include "Client/client.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    UserInterface::Interface::getInstance()->run();
+    clientSpace::Client c("localhost", 2323);
 
     return a.exec();
 }

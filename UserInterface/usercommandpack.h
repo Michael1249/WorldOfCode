596f717fc5f2a4945e7423f7cad7c160903b0d70
mgavryliuk <michaelgavriluk1@gmail.com>
2019-07-16 17:49:14 +0300

    upd UserInterface

--- UserInterface/usercommandpack.h
+++ UserInterface/usercommandpack.h
@@ -1,11 +1,22 @@
 #ifndef USERCOMMANDPACK_H
 #define USERCOMMANDPACK_H
 
+#include <QList>
+#include <memory>
+#include "usercommand.h"
+
+namespace UserInterface
+{
 
 class UserCommandPack
 {
 public:
     UserCommandPack();
+
+private:
+    QList<const std::unique_ptr<UserInterface::UserCommand>> mUser_command_list;
 };
+
+} // UserInterface
 
 #endif // USERCOMMANDPACK_H

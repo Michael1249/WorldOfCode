6dd114c0632b96979147a7bba31452be1fb4c0cf
7e1ght <7eght.jm@gmail.com>
2019-07-28 11:28:06 +0300

    add config

--- Client/client.cpp
+++ Client/client.cpp
@@ -1,5 +1,6 @@
-#include <qiostream.h>
 #include <QDataStream>
+#include <qiostream.h>
+
 #include "client.h"
 
 using namespace clientSpace;
@@ -9,7 +10,7 @@ Client::Client(const QString pHost)
 {
     mp_clientSocket = new QTcpSocket(this);
 
-    mp_clientSocket->connectToHost(pHost, 9999);
+    mp_clientSocket->connectToHost(pHost, ConfigSpace::PORT);
 
     connect(mp_clientSocket, SIGNAL(connected()), SLOT(slotConnected()));
     connect(mp_clientSocket, SIGNAL(error(QAbstractSocket::SocketError)), SLOT(slotError(QAbstractSocket::SocketError)));
@@ -59,23 +60,33 @@ void Client::slotReadServer()
 
         mi16_nextBlockSize = 0;
 
-        QIO::qout << "Server send: " << str << endl;
-        QIO::qout.flush();
+        qout << "Server send: " << str << endl;
+        qout.flush();
     }
 
     mp_clientSocket->flush();
 }
 
-void Client::sendToServer(const QString str)
+void Client::sendToServer(const QString str, ConfigSpace::sendType type)
 {
     mp_clientSocket->flush();
     QByteArray arrBlock;
     QDataStream out(&arrBlock, QIODevice::WriteOnly);
 
-    out << quint16(0) << str;
+    out << quint16(0) << type << str;
 
     out.device()->seek(0);
     out << quint16(arrBlock.size() - sizeof(quint16));
 
     mp_clientSocket->write(arrBlock);
+}
+
+void Client::sendToServer(QFile* file)
+{
+    if(file->open(QIODevice::ReadWrite))
+    {
+        sendToServer(file->readAll(), ConfigSpace::FILE);
+    } else {
+        qout << "Can not open file." << endl;
+    }
 }
--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 #include <QCoreApplication>
 #include <Client/client.h>
 #include <qiostream.h>
-#include <QFile>
 
 // Send data in little packages
 // Must open file in any mode
+using namespace QIO;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
-    using namespace QIO;
-
     clientSpace::Client c("localhost");
+
+    QFile file("test.txt");
+//    c.sendToServer(&file);
+    c.sendToServer("Vlad");
 
     return a.exec();
 }

75d10eaf5f8c3dbecd861db7c4a2510add8ad379
mgavryliuk <michaelgavriluk1@gmail.com>
2019-08-17 13:20:28 +0300

    reset main to default

--- main.cpp
+++ main.cpp
@@ -3,24 +3,9 @@
 #include "UIConfig.h"
 #include "qiostream.h"
 
-#include "jsonviewer.h"
-
-#include <QJsonObject>
-
-
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
-    JsonViewer jv;
-    A aa;
-
-    QObject::connect(&aa, SIGNAL(signalData(QJsonObject)), &jv, SLOT(readDoc(QJsonObject)));
-
-    aa.send("info", "data");
-    aa.send("warning", "data");
-    aa.send("error", "data");
-    aa.send("fatal", "data");
 
 //    setUpUI();
 //    UI::Run();
